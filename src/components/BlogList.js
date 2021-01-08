c1509e30a9ac52bb7c4dd145e30db67397e11b52
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-08 15:57:32 -0600

    useEffect to make a GET request

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const BlogList = ({ blogs, title, deleteBlog }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div>
       <h2>{title}:</h2>
@@ -8,7 +8,6 @@ const BlogList = ({ blogs, title, deleteBlog }) => {
         <div className='blog-preview' key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => deleteBlog(blog.id)}>Delete</button>
         </div>
       ))}
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      id: 1,
-      title: 'Welcome to the new blog',
-      body: 'lorem ipsum...',
-      author: 'mario',
-    },
-    {
-      id: 2,
-      title: 'Master React hooks',
-      body: 'lorem ipsum...',
-      author: 'luigi',
-    },
-    {
-      id: 3,
-      title: 'How to output lists in react',
-      body: 'lorem ipsum...',
-      author: 'mario',
-    },
-  ]);
+  const [blogs, setBlogs] = useState([]);
 
-  const onDelete = (id) => {
-    const updatedBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(updatedBlogs);
-  };
+  useEffect(() => {
+    fetch(`http://localhost:8000/blogs`)
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title='All blogs' deleteBlog={onDelete} />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === 'mario')}
-        title="Mario's blogs"
-      />
+      {blogs && <BlogList blogs={blogs} title='All blogs' />}
     </div>
   );
 };
