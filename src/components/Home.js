cfa529cfe92f4f48b3fe897dbf591e2bd99d8a99
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-12 20:37:48 -0600

    loading and error states

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,32 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(`http://localhost:8000/blogs`)
       .then((res) => res.json())
-      .then((data) => setBlogs(data));
+      .then((data) => {
+        setBlogs(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+        setError(err.message);
+      });
   }, []);
 
   return (
     <div className='home'>
-      {blogs && <BlogList blogs={blogs} title='All blogs' />}
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && blogs && (
+        <BlogList blogs={blogs} title='All blogs' />
+      )}
     </div>
   );
 };
